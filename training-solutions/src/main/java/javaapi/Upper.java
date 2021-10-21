70c226085a5f883d19978e95157121e79951c4fe
Emeriscus <kimre88@gmail.com>
2021-10-21 09:38:36 +0200

    Upper final v1.0

---
+++ training-solutions/src/main/java/javaapi/Upper.java
@@ -0,0 +1,16 @@
+package javaapi;
+
+public class Upper {
+    public static void main(String[] args) {
+
+        String hello = "Hello World!";
+
+        System.out.println(hello);
+
+        System.out.println(hello.toUpperCase());
+
+    }
+
+
+
+}
