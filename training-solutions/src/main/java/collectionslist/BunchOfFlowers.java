15dffb756ae6c6ed3073a29ff4ff025fc8df2bef
Emeriscus <kimre88@gmail.com>
2022-01-15 12:46:36 +0100

    collectionslist + test final v1.0

---
+++ training-solutions/src/main/java/collectionslist/BunchOfFlowers.java
@@ -0,0 +1,31 @@
+package collectionslist;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class BunchOfFlowers {
+
+    List<String> bunch = new LinkedList<>();
+
+    public List<String> getFlowers() {
+        return bunch;
+    }
+
+    public List<String> getBunch() {
+        return bunch;
+    }
+
+    public void addFlower(String flower) {
+        bunch.add(flower);
+    }
+
+    public void addFlowerInTheMiddle(String flower) {
+
+        if (bunch.size() % 2 == 0) {
+            bunch.add((bunch.size() / 2), flower);
+        } else {
+            bunch.add((bunch.size() / 2), flower);
+            bunch.add((bunch.size() / 2) + 1, flower);
+        }
+    }
+}
